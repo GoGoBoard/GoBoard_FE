ef4d51bf6b6bcd9586c10d58e323ad0d29ec381d
laireyx <laireyx@gmail.com>
2023-07-30 14:17:56 +0900

    feat: apply suspense to board page

---
+++ src/components/ArticleList.tsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+
+import {
+  Box,
+  Pagination,
+  Skeleton,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+
+import { listBoard } from '../api/board/list';
+
+function ArticleListHead() {
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell align="center">Index</TableCell>
+        <TableCell align="center">Title</TableCell>
+        <TableCell align="center">Author</TableCell>
+        <TableCell align="center">Date</TableCell>
+        <TableCell align="center">Likes</TableCell>
+      </TableRow>
+    </TableHead>
+  );
+}
+
+function SkeletonCell() {
+  return (
+    <TableCell align="center">
+      <Skeleton />
+    </TableCell>
+  );
+}
+
+export function ArticleListFallback() {
+  return (
+    <Stack spacing={2} alignItems={'center'} my={4}>
+      <Typography variant="h4">Board Page</Typography>
+      <TableContainer component={Box}>
+        <Table sx={{ minWidth: 'sm' }}>
+          <ArticleListHead />
+          <TableBody>
+            {Array(5).fill(
+              <TableRow hover>
+                <SkeletonCell />
+                <SkeletonCell />
+                <SkeletonCell />
+                <SkeletonCell />
+                <SkeletonCell />
+              </TableRow>,
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Stack>
+  );
+}
+
+export function ArticleList() {
+  const [page, setPage] = useState(0);
+
+  const navigate = useNavigate();
+
+  const { data } = useQuery({
+    queryKey: ['boardList', page],
+    queryFn: () => listBoard({ page }),
+    suspense: true,
+  });
+
+  return (
+    <Stack spacing={2} alignItems={'center'} my={4}>
+      <Typography variant="h4">Board Page</Typography>
+      <TableContainer component={Box}>
+        <Table sx={{ minWidth: 'sm' }}>
+          <ArticleListHead />
+          <TableBody>
+            {data?.data.map((article) => (
+              <TableRow
+                hover
+                key={article.index}
+                onClick={() => navigate(`./article/${article.index}`)}
+              >
+                <TableCell align="center">{article.index}</TableCell>
+                <TableCell align="center">{article.title}</TableCell>
+                <TableCell align="center">{article.author}</TableCell>
+                <TableCell align="center">
+                  {new Date(article.timestamp).toLocaleString()}
+                </TableCell>
+                <TableCell align="center">{article.likes}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Pagination
+        count={2}
+        page={page}
+        onChange={(_, newPage) => setPage(newPage)}
+      />
+    </Stack>
+  );
+}
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,72 +1,15 @@
-import { useState } from 'react';
+import { Suspense } from 'react';
 
-import {
-  Box,
-  Container,
-  Pagination,
-  Stack,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from '@mui/material';
-import { useQuery } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
+import { Container } from '@mui/material';
 
-import { listBoard } from '../api/board/list';
+import { ArticleList, ArticleListFallback } from '../components/ArticleList';
 
 export default function Board() {
-  const [page, setPage] = useState(0);
-  const navigate = useNavigate();
-
-  const { data } = useQuery({
-    queryKey: ['boardList', page],
-    queryFn: () => listBoard({ page }),
-  });
-
   return (
     <Container maxWidth="md">
-      <Stack spacing={2} alignItems={'center'} my={4}>
-        <Typography variant="h4">Board Page</Typography>
-        <TableContainer component={Box}>
-          <Table sx={{ minWidth: 'sm' }}>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">Index</TableCell>
-                <TableCell align="center">Title</TableCell>
-                <TableCell align="center">Author</TableCell>
-                <TableCell align="center">Date</TableCell>
-                <TableCell align="center">Likes</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data?.data.map((article) => (
-                <TableRow
-                  hover
-                  key={article.index}
-                  onClick={() => navigate(`./article/${article.index}`)}
-                >
-                  <TableCell align="center">{article.index}</TableCell>
-                  <TableCell align="center">{article.title}</TableCell>
-                  <TableCell align="center">{article.author}</TableCell>
-                  <TableCell align="center">
-                    {new Date(article.timestamp).toLocaleString()}
-                  </TableCell>
-                  <TableCell align="center">{article.likes}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Pagination
-          count={2}
-          page={page}
-          onChange={(_, newPage) => setPage(newPage)}
-        />
-      </Stack>
+      <Suspense fallback={<ArticleListFallback />}>
+        <ArticleList />
+      </Suspense>
     </Container>
   );
 }
